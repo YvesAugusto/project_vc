98efa27e85c32eca28378100b8c287c3fd40d987
yves <yvesromero1998@gmail.com>
2022-08-27 15:09:24 -0300

    changed argparse parameters name; chaged call for these parameters inside media, mediana and gaussiano codes .py

--- mediana.py
+++ mediana.py
@@ -10,17 +10,19 @@ import argparse
 
 PATH = "images/median/{}"
 parser = argparse.ArgumentParser()
-parser.add_argument('--filepath', '-filepath',
+parser.add_argument('--filepath', '-f',
                     type=str, help=parse.HELP_FILEPATH)
-parser.add_argument('--strides', '-strides',  default=(1, 1),
+parser.add_argument('--strides', '-s',  default=(1, 1),
                     type=parse.tuple_type, help=parse.HELP_STRIDES)
-parser.add_argument('--window_size', '-window_size',  default=(3, 3),
+parser.add_argument('--window_size', '-w',  default=(3, 3),
                     type=parse.tuple_type, help=parse.HELP_WINDOW)
-parser.add_argument('--rsz', '-resize_width',  default=200,
-                    type=parse.tuple_type, help=parse.HELP_RSZ)
-parser.add_argument('--padding', '-padding',  default='same', type=str,
+parser.add_argument('--resize_width', '-rsz',  default=200,
+                    type=int, help=parse.HELP_RSZ)
+parser.add_argument('--sigma', '-sig',  default=1.,
+                    type=float, help=parse.HELP_SIGMA)
+parser.add_argument('--padding', '-p',  default='same', type=str,
                     choices=['same', 'valid'], help=parse.HELP_PADDING)
-parser.add_argument('--svf', '-save_filename',  default=None,
+parser.add_argument('--save_filename', '-svf',  default=None,
                     type=str, help=parse.HELP_SAVE)
 
 matplotlib.use('TKAgg')
@@ -34,14 +36,14 @@ def calcula_mediana(window, kernel):
 if __name__ == '__main__':
     args = parser.parse_args()
     image = cv.imread(args.filepath, 0)
-    image = resize(image, 200)
+    image = resize(image, args.resize_width)
     dummy = np.zeros(args.window_size)
     median = conv.conv2d(
         img=image, kernel=dummy, strides=args.strides,
         padding=args.padding, prod_function=calcula_mediana
     )
-    filename = args.svf
-    if not args.svf:
+    filename = args.save_filename
+    if not args.save_filename:
         filename = "{}_median_{}x{}.png"
         filename = filename.format(
             args.filepath.split("/")[-1].split(".")[0],
